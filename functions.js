2bc8c60f7f0dae0f848b8620249b47e35918c004
Mariana Lourenço <slb.marianavitoria@gmail.com>
2024-10-31 15:07:28 -0300

    correção em functions

--- functions.js
+++ functions.js
@@ -1,11 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
 function openLink(url) {
     window.open(url, '_blank'); 
 }
-
-
-document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-        });
-});
