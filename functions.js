92dddee47238ce38fb0de4fc42d10a2e14dff90c
Mariana Lourenço <slb.marianavitoria@gmail.com>
2025-02-11 19:48:22 -0300

    modo escuro ativado e deixando mais clean

--- functions.js
+++ functions.js
@@ -1,5 +1,11 @@
+function openLink(url) {
+    window.open(url, '_blank'); 
+}
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Código para rolagem suave
     document.querySelectorAll('a').forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();
@@ -10,8 +16,29 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+
+    
+    const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
+    const imagem = document.querySelector('.img-animada');
+    const logoimg = document.querySelector('.img-logo')
+    
+    toggleDarkModeButton.addEventListener('click', function() {
+        // Alternar entre o modo escuro e claro
+        document.body.classList.toggle('dark-mode');
+    
+        // Alternar o texto do botão
+        if (document.body.classList.contains('dark-mode')) {
+            toggleDarkModeButton.textContent = 'Modo Claro';
+            imagem.src = 'profileDark.png';
+            logoimg.src = 'logomzDark.png';
+        } else {
+            toggleDarkModeButton.textContent = 'Modo Escuro';
+            imagem.src = 'profile.png'; 
+            logoimg.src = 'logomz.png';
+        }
+    });
+    
 });
 
-function openLink(url) {
-    window.open(url, '_blank'); 
-}
+
